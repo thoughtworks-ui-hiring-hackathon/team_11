9a39c981f702014c6b7201eff320f3d2321e1639
Omkar Bodke <omkar.bodke@IN-IT1477.local>
2019-06-21 13:01:37 +0530

    ob added empty components for home and explore along with angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { MovieCardComponent } from './shared/components/movie-card/movie-card.component';
-=======
 import { HeaderComponent } from './shared/components/header.component/header.component.component';
->>>>>>> 882fd5e25d6cc08fc3d53c0dd1e537e56f59104c
+import { HomeComponent } from './components/home/home.component';
+import { ExploreComponent } from './components/explore/explore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    MovieCardComponent
-=======
-    HeaderComponent
->>>>>>> 882fd5e25d6cc08fc3d53c0dd1e537e56f59104c
+    HeaderComponent,
+    HomeComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
